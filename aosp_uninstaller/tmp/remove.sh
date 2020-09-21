dcb7b39589c49d9a7d256a225e92baa8edf65759
tesla59 <nishant.maintainers@pixysos.com>
2020-09-22 02:25:48 +0530

    new zip aosp uninstaller with a prank xD

---
+++ aosp_uninstaller/tmp/remove.sh
@@ -0,0 +1,6 @@
+#!/sbin/sh
+rm -rf /system_root/system/product/app/Calender
+rm -rf /system_root/system/product/priv-app/Dialer
+rm -rf /system_root/system/product/app/messaging
+rm -rf /system_root/system/product/app/DeskClock
+rm -rf /system_root/system/product/priv-app/Contacts
